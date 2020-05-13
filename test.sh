222ccd6330cb5946573ae8547bffcd96914382eb
louiscklaw <louiscklaw@gmail.com>
2020-05-13 23:22:21 +0800

    update and retest,

--- test.sh
+++ test.sh
@@ -20,22 +20,22 @@ git checkout test/test_2
 pipenv sync
 pipenv run python3 ./merge.py
 
-git checkout feature/test_2
-# export TRAVIS_BRANCH=feature/test_2
-# export TRAVIS_COMMIT=0edef86
-# export TRAVIS_BUILD_NUMBER=1
-# export TRAVIS_REPO_SLUG=louiscklaw/test_on_github.git
-# export GITHUB_TOKEN=8c50e8410e3f2587b2680fdfca9e055ac4c8fbdc
+# git checkout feature/test_2
+# # export TRAVIS_BRANCH=feature/test_2
+# # export TRAVIS_COMMIT=0edef86
+# # export TRAVIS_BUILD_NUMBER=1
+# # export TRAVIS_REPO_SLUG=louiscklaw/test_on_github.git
+# # export GITHUB_TOKEN=8c50e8410e3f2587b2680fdfca9e055ac4c8fbdc
 
-pipenv sync
-pipenv run python3 ./merge.py
+# pipenv sync
+# pipenv run python3 ./merge.py
 
-git checkout pre-merge/test_2
-# export TRAVIS_BRANCH=pre-merge/test_2
-# export TRAVIS_COMMIT=0edef86
-# export TRAVIS_BUILD_NUMBER=1
-# export TRAVIS_REPO_SLUG=louiscklaw/test_on_github.git
-# export GITHUB_TOKEN=8c50e8410e3f2587b2680fdfca9e055ac4c8fbdc
+# git checkout pre-merge/test_2
+# # export TRAVIS_BRANCH=pre-merge/test_2
+# # export TRAVIS_COMMIT=0edef86
+# # export TRAVIS_BUILD_NUMBER=1
+# # export TRAVIS_REPO_SLUG=louiscklaw/test_on_github.git
+# # export GITHUB_TOKEN=8c50e8410e3f2587b2680fdfca9e055ac4c8fbdc
 
-pipenv sync
-pipenv run python3 ./merge.py
+# pipenv sync
+# pipenv run python3 ./merge.py
