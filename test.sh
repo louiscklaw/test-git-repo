0bf4f301c0435bbb427bbd267b5962dc5ad298a7
louiscklaw <louiscklaw@gmail.com>
2020-05-13 23:04:21 +0800

    update and retest,

--- test.sh
+++ test.sh
@@ -11,31 +11,31 @@ pip3 install --user pipenv
 
 git checkout test/test_2
 
-export TRAVIS_BRANCH=test/test_2
-export TRAVIS_COMMIT=0edef86
-export TRAVIS_BUILD_NUMBER=1
-export TRAVIS_REPO_SLUG=louiscklaw/test_on_github.git
-export GITHUB_TOKEN=8c50e8410e3f2587b2680fdfca9e055ac4c8fbdc
+# export TRAVIS_BRANCH=test/test_2
+# export TRAVIS_COMMIT=0edef86
+# export TRAVIS_BUILD_NUMBER=1
+# export TRAVIS_REPO_SLUG=louiscklaw/test_on_github.git
+# export GITHUB_TOKEN=8c50e8410e3f2587b2680fdfca9e055ac4c8fbdc
 
 pipenv sync
 pipenv run python3 ./merge.py
 
-
-export TRAVIS_BRANCH=feature/test_2
-export TRAVIS_COMMIT=0edef86
-export TRAVIS_BUILD_NUMBER=1
-export TRAVIS_REPO_SLUG=louiscklaw/test_on_github.git
-export GITHUB_TOKEN=8c50e8410e3f2587b2680fdfca9e055ac4c8fbdc
+git checkout feature/test_2
+# export TRAVIS_BRANCH=feature/test_2
+# export TRAVIS_COMMIT=0edef86
+# export TRAVIS_BUILD_NUMBER=1
+# export TRAVIS_REPO_SLUG=louiscklaw/test_on_github.git
+# export GITHUB_TOKEN=8c50e8410e3f2587b2680fdfca9e055ac4c8fbdc
 
 pipenv sync
 pipenv run python3 ./merge.py
 
-
-export TRAVIS_BRANCH=pre-merge/test_2
-export TRAVIS_COMMIT=0edef86
-export TRAVIS_BUILD_NUMBER=1
-export TRAVIS_REPO_SLUG=louiscklaw/test_on_github.git
-export GITHUB_TOKEN=8c50e8410e3f2587b2680fdfca9e055ac4c8fbdc
+git checkout pre-merge/test_2
+# export TRAVIS_BRANCH=pre-merge/test_2
+# export TRAVIS_COMMIT=0edef86
+# export TRAVIS_BUILD_NUMBER=1
+# export TRAVIS_REPO_SLUG=louiscklaw/test_on_github.git
+# export GITHUB_TOKEN=8c50e8410e3f2587b2680fdfca9e055ac4c8fbdc
 
 pipenv sync
 pipenv run python3 ./merge.py
