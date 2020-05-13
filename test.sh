45a06b7e9d872c4f95a547ca454944d5c484a25b
louiscklaw <louiscklaw@gmail.com>
2020-05-13 22:54:01 +0800

    update and test,

--- test.sh
+++ test.sh
@@ -2,6 +2,11 @@
 
 set -ex
 
+
+sudo apt update
+sudo apt install -y python3 python3-pip python3-dev python3-wheel python3-setuptools
+pip3 install --user pipenv
+
 # commit id 3956c9d
 
 git checkout test/test_2
