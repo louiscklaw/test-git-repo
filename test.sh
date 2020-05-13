8a67e2d713297cc20b218b896c8dc07bfe07be2d
louiscklaw <louiscklaw@gmail.com>
2020-05-14 01:26:05 +0800

    update and re-test,

--- test.sh
+++ test.sh
@@ -14,11 +14,11 @@ pip3 install --user pipenv
 
 git checkout test/test_2
 
-export TRAVIS_BRANCH=test/test_2
-export TRAVIS_COMMIT=0edef86
-export TRAVIS_BUILD_NUMBER=1
-export TRAVIS_REPO_SLUG=louiscklaw/test_on_github.git
-export GITHUB_TOKEN=5f96a076d65e0d11900bcee580db63f979e88497
+# export TRAVIS_BRANCH=test/test_2
+# export TRAVIS_COMMIT=0edef86
+# export TRAVIS_BUILD_NUMBER=1
+# export TRAVIS_REPO_SLUG=louiscklaw/test_on_github.git
+# export GITHUB_TOKEN=5f96a076d65e0d11900bcee580db63f979e88497
 
 git push --dry-run https://5f96a076d65e0d11900bcee580db63f979e88497@github.com/louiscklaw/test-git-repo
 
